be619eba5af403c3f1df079df8b196da51d592f0
jilsoneletropaul55-cyber <jilsoneletropaul55@gmail.com>
2025-09-10 16:53:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const form = document.getElementById('contatoForm');
+const msg = document.getElementById('msg');
+form.addEventListener('submit', function(e) {
+e.preventDefault();
+const nome = document.getElementById('nome').value.trim();
+const email = document.getElementById('email').value.trim();
+const mensagem = document.getElementById('mensagem').value.trim();
+if (nome && email && mensagem) {
+msg.textContent = `Obrigado, ${nome}! Sua mensagem foi enviada com sucesso.`;
+msg.style.color = "green";
+form.reset();
+} else {
+msg.textContent = "Por favor, preencha todos os campos.";
+msg.style.color = "red";
+}
+})
